5bd1dd40348443fc6aef296d6827fdcc98e4dbda
Thomas Valvik Yttri <thomasvalvikyttri@Thomass-MacBook-Pro.local>
2020-02-04 23:30:59 +0100

    Added remaining project startcode

--- src/System/Controller/Controller.java
+++ src/System/Controller/Controller.java
@@ -4,4 +4,50 @@ package System.Controller;
  * @Author Thomas V.Yttri
  */
 public class Controller {
+
+    private static int N = 5;
+
+    public static void main (String[] args){
+
+        Display display;
+        Sensor sensor;
+
+        RPCClient displayclient, sensorclient;
+
+        System.out.println("Controller starting ...");
+
+        RPCServerStopStub stopdisplay = new RPCServerStopStub();
+        RPCServerStopStub stopsensor = new RPCServerStopStub();
+
+        displayclient = new RPCClient(Common.DISPLAYHOST, Common.DISPLAYPORT);
+        sensorclient = new RPCClient(Common.SENSORHOST, Common.SENSORPORT);
+
+        // TODO: 2020-02-04
+        // Connect to sensor and display RPC servers
+        // Create local display and sensor objects
+        // Register display and sensor objects in the RPC layer
+
+        if (true){
+            throw new UnsupportedOperationException(TODO.method());
+        }
+
+        // Register stop methods in the RPC layer
+        displayclient.register(stopdisplay);
+        sensorclient.register(stopsensor);
+
+        // TODO: 2020-02-04
+        // Loop while reading from sensor and write to display via RPC
+
+        if (true){
+            throw new UnsupportedOperationException(TODO.method());
+        }
+
+        stopdisplay.stop();
+        stopsensor.stop();
+
+        displayclient.disconnect();
+        sensorclient.disconnect();
+
+        System.out.println("Controller stopping ...");
+    }
 }
--- src/System/Display/DisplayDevice.java
+++ src/System/Display/DisplayDevice.java
@@ -4,4 +4,19 @@ package System.Display;
  * @Author Thomas V.Yttri
  */
 public class DisplayDevice {
+
+    public static void main(String[] args){
+
+        System.out.println("Display server starting ...");
+
+        // TODO: 2020-02-04
+        // Implement the operation of the display RPC server
+        // See how this is done for the sensor RPC server in SensorDevice
+
+        if (true){
+            throw new UnsupportedOperationException(TODO.method());
+        }
+
+        System.out.println("Display server stopping ...");
+    }
 }
